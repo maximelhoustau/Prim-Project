03380b278e61a96bacd7df2c44980b3a31fbf052
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2019-12-03 15:36:26 +0100

    Test for the timeline, adding video test and timeline_test_ref. Output : list of time and status of field. Need to apply mask to avoir noise

--- Extract_frame.py
+++ Extract_frame.py
@@ -1,10 +1,17 @@
 import cv2
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help = "path to the video file")
+ap.add_argument("-i", "--image", help = "path to the image file saved")
+ap.add_argument("-t", "--time", type=int, help = "time corresponding to the extracted frame")
 
-vidcap = cv2.VideoCapture('./videos/test.mp4')
-time = 1000*6 + 27*60*1000
+args = vars(ap.parse_args())
+
+vidcap = cv2.VideoCapture(args["video"])
+time = args["time"]*1000
 vidcap.set(cv2.CAP_PROP_POS_MSEC,time)      # just cue to 20 sec. position
 success,image = vidcap.read()
 if success:
-    cv2.imwrite("./images/fieldref.jpg", image)     # save frame as JPEG file
-    cv2.imshow(str(time)+" sec",image)
+    cv2.imwrite(args["image"], image)     # save frame as JPEG file
+    cv2.imshow(str(time)+"sec",image)
     cv2.waitKey()
--- Motion-Detection.py
+++ Motion-Detection.py
@@ -10,99 +10,123 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-i", "--initialization", help="set whether to initilize with a picture (provide path) or default with the first frame of the video")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-d", "--display", type=int, default=1, help="minimum area size")
 
 args = vars(ap.parse_args())
 
 image_folder = "./images/"
 video_folder = "./videos/"
 
+timeline = []
+event = False
+
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
-	vs = VideoStream(src=0).start()
-	time.sleep(2.0)
+        vs = VideoStream(src=0).start()
+        time.sleep(2.0)
 
 # otherwise, we are reading from a video file
 else:
-	vs = cv2.VideoCapture(video_folder + args["video"])
+        vs = cv2.VideoCapture(video_folder + args["video"])
 
 # initialize the background as a picture
 if args.get("initialization", None) is None:
-	firstFrame = None
+        firstFrame = None
 
 # otherwise, we initialize the frame to none and we'll take the first frame of the video as
 # background
 else:
-	firstFrame = cv2.imread(image_folder + args["initialization"])
-	firstFrame = imutils.resize(firstFrame, width=800)
-	firstFrame = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
-	firstFrame = cv2.GaussianBlur(firstFrame, (21, 21), 0)
+        firstFrame = cv2.imread(image_folder + args["initialization"])
+        firstFrame = imutils.resize(firstFrame, width=800)
+        firstFrame = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
+        firstFrame = cv2.GaussianBlur(firstFrame, (21, 21), 0)
+
+# Get the fps and number of frame to get the time of the video
+fps = vs.get(cv2.CAP_PROP_FPS)
+count = 0
 
 # loop over the frames of the video
 while True:
-	# grab the current frame and initialize the occupied/unoccupied
-	# text
-	frame = vs.read()
-	#print(frame.shape)
-	frame = frame if args.get("video", None) is None else frame[1]
-	text = "Unoccupied"
+        # grab the current frame and initialize the occupied/unoccupied
+        # text
+        frame = vs.read()
 
-	# if the frame could not be grabbed, then we have reached the end
-	# of the video
-	if frame is None:
-		break
+        #print(frame.shape)
+        frame = frame if args.get("video", None) is None else frame[1]
+        text = "Unoccupied"
 
-	# resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width=800)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
-	
-	# if the first frame is None, initialize it
-	if firstFrame is None:
-		firstFrame = gray
-		continue
+        # if the frame could not be grabbed, then we have reached the end
+        # of the video
+        if frame is None:
+                break
 
-	# first frame
-	frameDelta = cv2.absdiff(firstFrame, gray)
-	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+        # resize the frame, convert it to grayscale, and blur it
+        frame = imutils.resize(frame, width=800)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        
+        # if the first frame is None, initialize it
+        if firstFrame is None:
+                firstFrame = gray
+                continue
+
+        # first frame
+        frameDelta = cv2.absdiff(firstFrame, gray)
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
  
-	# dilate the thresholded image to fill in holes, then find contours
-	# on thresholded image
-	thresh = cv2.dilate(thresh, None, iterations=2)
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
- 
-	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < args["min_area"]:
-			continue
- 
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Occupied"
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+    
+        #Set by default value of event_in at False (if non contours)
+        event_in = False
+        # loop over the contours
+        for c in cnts:
+                # if the contour is too small, ignore it
+                if cv2.contourArea(c) < args["min_area"]:
+                        continue
+                
+                event_in = True
+                # compute the bounding box for the contour, draw it on the frame,
+                # and update the text
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                text = "Occupied"
 
-		# draw the text and timestamp on the frame
-	cv2.putText(frame, "Field status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        #Print the timeline when the status of the field change
+        if(event ^ event_in):
+                event = event_in
+                timeline.append([count/fps, event])
 
-	# show the frame and record if the user presses a key
-	cv2.imshow("Frame", frame)
-	cv2.imshow("Thresh", thresh)
-	cv2.imshow("Frame Delta", frameDelta)
-	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key is pressed, break from the lop
-	if key == ord("q"):
-		break
-	    
-	if key == ord("w"):
-	    cv2.imwrite(image_folder+"MD_frame.jpg", frame)
-	    cv2.imwrite(image_folder+"MD_thresh.jpg", thresh)
-	    cv2.imwrite(image_folder+"MD_delta.jpg", frameDelta)
+        # draw the text and timestamp on the frame if display is set at false
+        if (args["display"]):
+
+            cv2.putText(frame, "Field status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+            # show the frame and record if the user presses a key
+            cv2.imshow("Frame", frame)
+            cv2.imshow("Thresh", thresh)
+            cv2.imshow("Frame Delta", frameDelta)
+
+        # if the `q` key is pressed, break from the lop
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+                break
+            
+        if key == ord("w"):
+            cv2.imwrite(image_folder+"MD_frame.jpg", frame)
+            cv2.imwrite(image_folder+"MD_thresh.jpg", thresh)
+            cv2.imwrite(image_folder+"MD_delta.jpg", frameDelta)
+        
+        #Count for next frame
+        count += 1
 
 # cleanup the camera and close any open windows
 vs.stop() if args.get("video", None) is None else vs.release()
 cv2.destroyAllWindows()
+print(timeline)
