a2b5012f6289261935ba6bc330bdfbd8ea7be91c
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2019-12-06 18:24:20 +0100

    Add comparisons between GME techniques and plot

--- Motion-Detection-Flow.py
+++ Motion-Detection-Flow.py
@@ -1,6 +1,5 @@
 from imutils.video import VideoStream
 import argparse
-import datetime
 import imutils
 import time as T
 import cv2
@@ -8,53 +7,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.Apply_mask import apply_mask
 
-
-start_time = T.time()
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 ap.add_argument("-d", "--display", type=int, default=1, help="minimum area size")
+ap.add_argument("-r", "--resized", type=int, default=0, help="Set the width to resize the image")
 
 args = vars(ap.parse_args())
+
+displayed = args["display"]
+video_title = args["video"]
+resized = args["resized"]
 
 image_folder = "./images/"
 video_folder = "./videos/"
 
-timeline = []
-motion = []
-event = False
-time = 0
+print("Video: "+video_folder+video_title)
+print("Displayed : "+str(displayed))
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
+def MDF(masked, resized):
+    start_time = T.time()
+    timeline = []
+    motion = []
+    time_t = 0
+
+    # if the video argument is None, then we are reading from webcam
+    if args.get("video", None) is None:
         vs = VideoStream(src=0).start()
         time.sleep(2.0)
 
-# otherwise, we are reading from a video file
-else:
-        vs = cv2.VideoCapture(video_folder + args["video"])
+    # otherwise, we are reading from a video file
+    else:
+        vs = cv2.VideoCapture(video_folder + video_title)
 
-# Get the fps and number of frame to get the time of the video
-fps = vs.get(cv2.CAP_PROP_FPS)
-count = 0
+    # Get the fps and number of frame to get the time of the video
+    fps = vs.get(cv2.CAP_PROP_FPS)
+    count = 0
 
-# loop over the frames of the video
-frame = vs.read()[1]
-while True:
-        # grab the current frame 
-        frame_t = vs.read()[1] 
+    # loop over the frames of the video
+    frame = vs.read()[1]
+    frame_t = None
+    while True:
+        time = count/fps
+        grabbed = vs.grab()
+        if(grabbed):          
+            count += 1
+            if((time - time_t) >= 0.07):
+                # grab the current frame 
+                frame_t = vs.retrieve()[1] 
+                time_t = time
+            else:
+                continue
+        else:
+            break
+        
+        #Apply masked or not
+        if(masked): 
+            frame = apply_mask(frame) 
+            frame_t = apply_mask(frame_t) 
+        
+        #Apply resized or not
+        if(resized):
+            frame = imutils.resize(frame, width=500)
+            frame_t = imutils.resize(frame_t, width=500)       
 
-        # if the frame could not be grabbed, then we have reached the end
-        # of the video
-        if frame_t is None:
-                break
 
-        # resize the frame, convert it to grayscale, and blur it
-        #frame = imutils.resize(frame, width=800)
-        #frame_t = imutils.resize(frame_t, width=800)
-        frame = apply_mask(frame) 
-        frame_t = apply_mask(frame_t) 
+        # convert it to grayscale, and blur it
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray_t = cv2.cvtColor(frame_t, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -66,10 +84,10 @@ while True:
  
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
-        thresh = cv2.dilate(thresh, None, iterations=4)
+        #thresh = cv2.dilate(thresh, None, iterations=2)
     
         #Show the real time videos if display is not set at 0
-        if (args["display"]):
+        if (displayed):
 
             # show the frame and record if the user presses a key
             cv2.imshow("Frame", frame)
@@ -86,27 +104,54 @@ while True:
             cv2.imwrite(image_folder+"MD_thresh.jpg", thresh)
             cv2.imwrite(image_folder+"MD_delta.jpg", frameDelta)
         
-        #Count for next frame
-        count += 1
-
         #Add time and motion counter of this frame to their vector in order to plot
         timeline.append(count/fps)  
         motion.append(np.count_nonzero(thresh))
         #update frame
         frame = frame_t
 
-# cleanup the camera and close any open windows
-vs.stop() if args.get("video", None) is None else vs.release()
-cv2.destroyAllWindows()
+    # cleanup the camera and close any open windows
+    vs.stop() if args.get("video", None) is None else vs.release()
+    cv2.destroyAllWindows()
+    
+    print("\nMasked : "+str(masked))
+    print("Size of the frames : "+str(frame.shape))
+    print("Execution time : "+str(T.time() - start_time))
+    
+    return(timeline, motion)
 
-print("Execution time : "+str(T.time() - start_time))
 
-#Plot the motion timeline
-fig, ax = plt.subplots()
-ax.plot(timeline, motion)
-ax.set(xlabel='Time (ms)', ylabel='Global Motion Estimation (pixel)', title='Global Motion Estimation over Time')
-ax.grid()
-fig.savefig(image_folder+"GME.png")
+fig, ax = plt.subplots(2, 2, sharex='col', sharey='row')
+
+timeline, motion = MDF(masked = 0, resized = 0)
+ax[0,0].plot(timeline, motion)
+ax[0,0].set(ylabel='GME (pixels)', title='Video Unmasked and original size')
+ax[0,0].grid(True)
+
+timeline, motion = MDF(masked = 1, resized = 0)
+ax[0,1].plot(timeline, motion)
+ax[0,1].set(title = 'Video Masked and original size')
+ax[0,1].grid(True)
+
+timeline, motion = MDF(masked = 0, resized = resized)
+ax[1,0].plot(timeline, motion)
+ax[1,0].set(xlabel='Time (ms)', ylabel='GME (pixels)', title = 'Video Unmasked and resized to '+str(resized))
+ax[1,0].grid(True)
+
+timeline, motion = MDF(masked = 1, resized = resized)
+ax[1,1].plot(timeline, motion)
+ax[1,1].set(xlabel='Time (ms)', ylabel='GME (pixels)', title = 'Video Masked and resized to '+str(resized))
+ax[1,1].grid(True)
+
 plt.show()
+fig.savefig(image_folder+"GME_comparison_"+str(resized)+".png")
+
+
+
+
+
+
+
+
 
 
